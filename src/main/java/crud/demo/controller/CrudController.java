27ded6cc472631f88366c7ac604e58d220f7148a
ryukyungwoo <ryukyungwoo1220@gmail.com>
2024-06-20 05:17:59 +0900

    fix : [유경우] Cors 설정 한정 및 CreateForm에 @Data 누락 수정 [FCB - 2]

--- src/main/java/crud/demo/controller/CrudController.java
+++ src/main/java/crud/demo/controller/CrudController.java
@@ -4,16 +4,17 @@ import crud.demo.controller.requestForm.CreateForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CrudController {
     private static final Logger log = LoggerFactory.getLogger(CrudController.class);
 
     @PostMapping("/create")
-    public void create(CreateForm createForm) {
-        log.info(createForm.getTitle());
-        log.info(createForm.getPassword());
-        log.info(createForm.getContent());
+    public void create(@RequestBody CreateForm createForm) {
+        log.info(createForm.toString());
     }
 }
--- src/main/java/crud/demo/controller/requestForm/CreateForm.java
+++ src/main/java/crud/demo/controller/requestForm/CreateForm.java
@@ -2,21 +2,10 @@ package crud.demo.controller.requestForm;
 
 import lombok.*;
 
+@Getter
 @ToString
 public class CreateForm {
     private String title;
     private String password;
     private String content;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getContent() {
-        return content;
-    }
 }
